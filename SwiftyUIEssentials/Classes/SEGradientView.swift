d64912af99ee8bff82e4193b4611f8f1d89a6440
Josue Meza Peña <contacto@josuemeza.com>
2018-08-14 16:29:19 -0300

    added gradient sample and minor SEGradientView fixes

--- SwiftyUIEssentials/Classes/SEGradientView.swift
+++ SwiftyUIEssentials/Classes/SEGradientView.swift
@@ -63,7 +63,8 @@ import UIKit
      - Parameter endPoint: Gradient end point.
      */
     public func gradient(startColor: UIColor, endColor: UIColor, startPoint: CGPoint, endPoint: CGPoint) {
-        gradientColors = [startColor, endColor]
+        gradientStartColor = startColor
+        gradientEndColor = endColor
         gradientStartPoint = startPoint
         gradientEndPoint = endPoint
     }
@@ -77,7 +78,7 @@ import UIKit
      
      - Parameter actions: Actions to set on gradient layer.
      */
-    private func gradientDefaults(action: @escaping () -> Void) {
+    private func gradientDefaults(action: @escaping () -> Void = {}) {
         gradientLayer.frame = bounds
         if let current = layer.sublayers?.first, current == gradientLayer {
             current.removeFromSuperlayer()
@@ -85,6 +86,12 @@ import UIKit
         action()
         layer.insertSublayer(gradientLayer, at: 0)
         layer.masksToBounds = true
+    }
+    
+    /// Overrided layout subviews method for apply on autolayout.
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        gradientDefaults()
     }
 
 }
